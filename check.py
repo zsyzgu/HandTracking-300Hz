945f68f0fa120e3daa2e3f4d89d01e0bca2b8614
zsyzgu <zsyzgu@163.com>
2021-11-16 16:33:33 +0800

    Sync Board and Camera.

--- check.py
+++ check.py
@@ -1,7 +1,11 @@
+from time import sleep
 import cv2
 import pickle
+import time
 
-def input(load_path):
+def main(load_path):
+    # ===== Input =====
+
     board_cap = cv2.VideoCapture(load_path + 'board.avi')
     board_frames = []
     while True:
@@ -22,7 +26,23 @@ def input(load_path):
     board_contacts = pickle.load(open(load_path + 'board_contacts.pickle', 'rb'))
     camera_timestamps = pickle.load(open(load_path + 'camera_timestamps.pickle', 'rb'))
 
-    print(len(board_frames), len(board_timestamps), len(board_contacts), len(camera_frames), len(camera_timestamps))
+    # ===== Illustration =====
+
+    speed = 0.1
+    FPS = 100
+    t = 0
+    board_cnt = 0
+    camera_cnt = 0
+    while t <= board_timestamps[-1] and t <= camera_timestamps[-1]:
+        while board_cnt + 1 < len(board_timestamps) and board_timestamps[board_cnt + 1] < t:
+            board_cnt += 1
+        while camera_cnt + 1 < len(camera_timestamps) and camera_timestamps[camera_cnt + 1] < t:
+            camera_cnt += 1
+        cv2.imshow('board', board_frames[board_cnt])
+        cv2.imshow('camera', camera_frames[camera_cnt])
+        cv2.waitKey(int(1000.0 / FPS))
+        t += (1.0 / FPS) * speed
 
 if __name__ == '__main__':
-    input('data/')
+    main('data/')
+    
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import pickle
 import time
 from camera import Camera
 from board import Board
@@ -16,5 +17,13 @@ if __name__ == '__main__':
     p_camera = multiprocessing.Process(target=process_camera, args=(save_path,))
     p_board = multiprocessing.Process(target=process_board, args=(save_path,))
     p_camera.start()
+    board_start_time = time.perf_counter()
     p_board.start()
+    gap = time.perf_counter() - board_start_time
+    p_camera.join()
+    p_board.join()
+    save_file = save_path + 'board_timestamps.pickle' # Sync the timestamps
+    board_timestamps = pickle.load(open(save_file, 'rb'))
+    board_timestamps = [t + gap for t in board_timestamps]
+    pickle.dump(board_timestamps, open(save_file, 'wb'))
     
